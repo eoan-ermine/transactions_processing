0768c1b9bdf4bde7063ef172710ac28b388591dc
PatriotRossii <patriotrossii2019@mail.ru>
2023-06-08 21:44:53 +0300

    Populate database with transactions information
    * Add psycopg2 and python-dotenv as a dependency
    * Create config.py
    * Implement logic in __main__.py

--- transactions_processing/__main__.py
+++ transactions_processing/__main__.py
@@ -1,7 +1,13 @@
+# pylint ошибочно выдает ошибку, что не может найти psycopg2 и transactions_processing.config
+# pylint: disable=import-error
+
 import csv
 import itertools
 
+import psycopg2
 import requests
+
+from transactions_processing.config import db_host, db_name, db_password, db_port, db_user
 
 source_files = ["clients.csv", "currency.csv", "amount.csv"]
 contents = []
@@ -46,3 +52,39 @@ for idx in range(transactions_count):
     name, last_name, middle_name = transactions[idx][CLIENTS_IDX].split()
     if len(last_name) > 8:
         transactions[idx][CLIENTS_IDX] = f"{last_name} {name[0]}.{middle_name[0]}."
+
+# Подключение к базе данных
+conn = psycopg2.connect(dbname=db_name, user=db_user, password=db_password, host=db_host, port=db_port)
+cur = conn.cursor()
+
+# Занесем в базу данных информацию о всех клиентах
+for client in contents[CLIENTS_IDX]:
+    cur.execute('INSERT INTO "public.Client" (identifier) VALUES (%s) ON CONFLICT DO NOTHING', (client,))
+# Завершим транзакцию
+conn.commit()
+
+# Занесем в базу данных информацию о всех валютах
+for currency in contents[CURRENCY_IDX]:
+    cur.execute('INSERT INTO "public.Currency" (code) VALUES (%s) ON CONFLICT DO NOTHING', (currency,))
+# Завершим транзакцию
+conn.commit()
+
+# Занесем в базу данных информацию о всех транзакциях
+for transaction in transactions:
+    client, currency, amount = transaction
+
+    cur.execute(
+        'INSERT INTO "public.Transaction" (client, currency, amount) VALUES(%s, %s, %s) RETURNING id',
+        (client, currency, amount),
+    )
+    transaction_id = cur.fetchone()[0]
+
+    # Заодно обновим BigTransaction/UsualTransaction
+    # BigTransaction — события из списка, сумма транзакций которых больше 1000
+    # BigTransaction — события из списка, сумма транзакций которых меньше или равна 1000
+    if amount > 1000:
+        cur.execute('INSERT INTO "public.BigTransaction" (transaction_id) VALUES(%s)', (transaction_id,))
+    else:
+        cur.execute('INSERT INTO "public.UsualTransaction" (transaction_id) VALUES(%s)', (transaction_id,))
+# Завершим транзакцию
+conn.commit()
---
+++ transactions_processing/config.py
@@ -0,0 +1,11 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
